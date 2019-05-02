d67e1a540ad7d624d8e86f257145564f8d11f703
wazen2018 <wazen@qq.com>
2019-05-02 11:13:14 +0800

    欢迎页面 服务器数据库php信息获取

--- application/admin/config.php
+++ application/admin/config.php
@@ -3,6 +3,7 @@ return [
 
 'view_replace_str'  =>  [
     '__ADMIN__'=>'/static/admin',
+    '__JS__'=>'/static/js',
    
 ]
 
--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,10 +1,43 @@
 <?php
 namespace app\admin\controller;
+use think\Db;
 
 class Index extends Common
 {
     public function index()
     {
+        return view();
+    }
+    
+
+    public function welcome()
+    {
+       // dump($_SERVER);
+       //读取服务器信息 5.2 
+        $server=[
+            'SERVER_PORT'=>$_SERVER["SERVER_PORT"],
+            'SERVER_ADDR'=>$_SERVER["SERVER_ADDR"],
+            'SERVER_NAME'=>$_SERVER["SERVER_NAME"],
+            'OSNAME'=>php_uname(),
+            'HTTP_ACCEPT_LANGUAGE'=>$_SERVER["HTTP_ACCEPT_LANGUAGE"],
+            'HTTP_HOST'=>$_SERVER["HTTP_HOST"],
+
+        ];
+
+        //原声查询   引入db类
+        $version=Db::query("select version()");
+        $server['mysqlversion']=$version[0]['version()'];
+        //读取配置文件 config
+        $server['database']=config('database.database');
+        //php版本
+        $server['phpversion']=phpversion();
+        //最大上传大小
+        $server['max_file_uploads']=ini_get('max_file_uploads');
+
+
+
+
+        $this->assign('server',$server);
         return view();
     }
 }
