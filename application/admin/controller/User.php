bc4fff162258acee7200416bf51e89d64e4f03f7
wazen2018 <wazen@qq.com>
2019-05-09 10:30:06 +0800

    完成上传tp5的方式

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -1,5 +1,7 @@
 <?php
 namespace app\admin\controller;
+use think\Request;
+use think\File;
 
 class User extends Common
 {
@@ -91,9 +93,33 @@ class User extends Common
 	public function upload()
 	{
 		if(request()->isPost()){
-			return js_encode("处理成功");
+		 // 获取表单上传文件 例如上传了001.jpg
+		 $file = request()->file('image');
+    
+		 // 移动到框架应用根目录/public/uploads/ 目录下
+		 if($file){
+			 $info = $file->validate(['size'=>10485760,'ext'=>'xls,xlsx'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+			 if($info){
+				 // 成功上传后 获取上传信息
+				 // 输出 jpg
+				 echo "上传成功<br>";
+				 echo $info->getExtension()."<br>";
+				 // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+				 echo $info->getSaveName()."<br>";
+				 // 输出 42a79759f284b767dfcb2a0197904287.jpg
+				 echo $info->getFilename(); 
+			 }else{
+				 // 上传失败获取错误信息
+				 echo $file->getError();
+			 }
+		 }
 		}
-	    return view();
+		
+		return view();
+		
+
+
+		
 	}
 	
 }
